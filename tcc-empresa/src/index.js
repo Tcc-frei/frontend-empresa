752478fc9bde5655811c3a6e6bd88fd4f40504d1
Roberto <betinhosp2007@gmail.com>
2024-09-24 11:46:56 -0300

    atualização

---
+++ tcc-empresa/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Rotas from './rotas'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Rotas/>
+  </React.StrictMode>
+);
+
---
+++ tcc-empresa/src/rotas.jsx
@@ -0,0 +1,14 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import App from './App'
+import Empresa from './landingpage'
+
+export default function Rotas(){
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path="/teste" element={<App/>}/>
+                <Route path="/" element={<Empresa/>}/>
+            </Routes>
+        </BrowserRouter>
+    )
+}
