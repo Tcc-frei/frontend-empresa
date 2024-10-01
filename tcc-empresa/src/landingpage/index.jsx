6137b5fc37e09184bcf15ed0df1685d4ec22ea1a
roberto <betinhosp2007@gmail.com>
2024-10-01 09:13:41 -0300

    atualizacao

--- tcc-empresa/src/landingpage/index.jsx
+++ tcc-empresa/src/landingpage/index.jsx
@@ -1,6 +1,11 @@
 import "./index.scss";
 
 import Cabecalho from "../components/cabecalho";
+import logo_html from "../assets/html.png"
+import logo_css from "../assets/css.png"
+import logo_nuvem from "../assets/nuvem.png"
+import logo_js from "../assets/javascript.png"
+import logo_bd from "../assets/bd.png"
 
 export default function () {
   return (
@@ -9,7 +14,15 @@ export default function () {
       <Cabecalho/>
 
       <div className="container-sobre-nos">
-        <h1>fasdf</h1>
+        <div className="simbolos">
+          <img className="html" src={logo_html} alt="" />
+          <img className="nuvem" src={logo_nuvem} alt="" />
+          <img className="bd" src={logo_bd} alt="" />
+          <img className="js" src={logo_js} alt="" />
+          <img className="css" src={logo_css} alt="" />
+        <h1>{`<h1>`}uma empresa renomada de T.I{`<h1/>`}</h1>
+        </div>
+        
       </div>
     </div>
   );
